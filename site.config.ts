76052f656cc2a7d6517dc3b91ca0e390f6eec70d
卓忆科技 <zykj@users.noreply.github.com>
2022-07-26 13:19:44 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -17,9 +17,9 @@ export default siteConfig({
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'stevezhang',
+  github: 'zykj',
+  linkedin: 'hao-zhang-3b967566',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 

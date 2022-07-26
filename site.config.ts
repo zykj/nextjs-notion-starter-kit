48d0b1e39fcf298fd67d5669b593a7ffd1ab7412
卓忆科技 <zykj@users.noreply.github.com>
2022-07-26 11:39:13 +0800

    更新notion信息

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '64733d22391543c3a9d1d578c15a2852',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Hello World',
+  domain: 'https://pchx.notion.site/Notion-Block-64733d22391543c3a9d1d578c15a2852',
+  author: 'PCX',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
